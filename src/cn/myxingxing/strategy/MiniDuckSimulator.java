c04f8998fd22a5fd78009933471fefb5f404fb64
LiXing <myxingxing66@126.com>
2016-11-10 08:50:23 +0800

    动态设定行为

--- src/cn/myxingxing/strategy/MiniDuckSimulator.java
+++ src/cn/myxingxing/strategy/MiniDuckSimulator.java
@@ -2,8 +2,12 @@ package cn.myxingxing.strategy;
 
 public class MiniDuckSimulator {
 	public static void main(String[] args) {
-		Duck mallard = new MallardDuck();
-		mallard.performQuack();
-		mallard.performFly();
+//		Duck mallard = new MallardDuck();
+//		mallard.performQuack();
+//		mallard.performFly();
+		Duck model = new ModelDuck();
+		model.performFly();
+		model.setFlyBehavior(new FlyRocketPowered());
+		model.performFly();
 	}
 }
